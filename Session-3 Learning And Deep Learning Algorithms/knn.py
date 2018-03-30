1407805e42ace41979151845fd844d2cddf3ea07
prateek951 <prince.cenation@gmail.com>
2018-03-30 16:30:30 +0530

    using knn fit and predict

--- Session-3 Learning And Deep Learning Algorithms/knn.py
+++ Session-3 Learning And Deep Learning Algorithms/knn.py
@@ -2,3 +2,22 @@
 
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+
+# Matrix of features
+
+fruit_feature = np.array([[1,9],[2,8],[3,7],[2,8],[6,4],[9,1],[8,2],[9,1],[1,1],[5,5]])
+
+# Dependent vector
+
+fruit_target = np.array(["Sour","Sour","Sour","Sour","Sweet","Sweet","Sweet","Sour","Sweet","Sweet"])
+
+# Create the object for the knn classifier
+
+myKNN = KNeighborsClassifier()
+
+# Fit the model to the training data
+
+myKNN.fit(fruit_feature,fruit_target)
+
+# Using the predict method to predict the output
+myKNN.predict([7,3])
