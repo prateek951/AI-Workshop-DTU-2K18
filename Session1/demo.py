448d292651fa083c8e2dbfbebaf8ceff09164fcd
prateek951 <prince.cenation@gmail.com>
2018-03-30 15:19:33 +0530

    working with arrays

--- Session1/demo.py
+++ Session1/demo.py
@@ -5,3 +5,17 @@ import numpy as np
 # Creating the array using numpy
 
 myarray = np.array([12,2,2,2,2,2])
+
+print(myarray)
+
+# The type of the array is np.ndarray whereas in python it would be list class
+print(type(myarray))
+
+# To print the order of the matrix
+print(myarray.ndim);
+
+# To print the shape of the array
+print(myarray.shape)
+
+# To print a specific element of the array
+print(myarray[0])
