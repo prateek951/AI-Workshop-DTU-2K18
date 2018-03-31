a9eb3f34759a51c6f5e1980b91b37a9c091a80f9
prateek951 <prince.cenation@gmail.com>
2018-03-31 10:52:53 +0530

    working with params

--- Session-3 Learning And Deep Learning Algorithms/iris.py
+++ Session-3 Learning And Deep Learning Algorithms/iris.py
@@ -1,7 +1,20 @@
 from sklearn import datasets
 
+#Load the dataset
+
 dataset = datasets.load_iris()
 
 # To check the shape for the dataset
 
 print(dataset.data.shape)
+
+# To check the keys for a dictionary
+print(dataset.keys())
+print(dataset.target)
+print(dataset.target_names)
+print(dataset.target_names[0])
+print(dataset.target_names[1])
+
+# The features of the iris dataset
+print(dataset.feature_names)
+
