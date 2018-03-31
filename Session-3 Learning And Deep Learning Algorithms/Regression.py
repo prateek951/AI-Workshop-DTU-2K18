86350c8a21c598f563450bb4bfee73ba51161645
prateek951 <prince.cenation@gmail.com>
2018-03-31 17:38:02 +0530

    predictions for the test set

--- Session-3 Learning And Deep Learning Algorithms/Regression.py
+++ Session-3 Learning And Deep Learning Algorithms/Regression.py
@@ -20,7 +20,7 @@ Y_train = Y[:-250]
 X_test = X[-250:]
 Y_test = Y[-250:]
 
-plt.scatter(X_test,Y_test,color='blue',marker='*')
+plt.scatter(X_train,Y_train,color='blue',marker='*')
 plt.title('Linear Regression Model')
 plt.xlabel('Lotsize')
 plt.ylabel('Price')
@@ -30,3 +30,9 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train,Y_train)
 print(regressor.predict(5000))
+
+# Predictions of price for the lotsize in the test set
+plt.scatter(X_test,regressor.predict(X_test))
+plt.xlabel('Lotsize for the testing set')
+plt.ylabel('Price for the testing set')
+plt.show()
