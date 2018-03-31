89a70537777cf836ef678c29113632d4cd655351
prateek951 <prince.cenation@gmail.com>
2018-03-31 11:43:27 +0530

    knn done predicts correct

--- Session-3 Learning And Deep Learning Algorithms/iris.py
+++ Session-3 Learning And Deep Learning Algorithms/iris.py
@@ -38,4 +38,14 @@ knn.fit(X_train,Y_train)
 # Predict class for the new data point
 print(knn.predict([[0.0,2.2,1.1,1.1]]))
 
+# Predicted by the model on the test values
+print(knn.predict(X_test))
+
+# Predict for the values that are there in the dataset
+
+print(knn.predict([[6.7,3.1,4.4,1.4]]))
+
+# More predictionss
+print(knn.predict([[5.7,2.5,5,2]]))
+print(knn.predict([[5,3.6,1.4,.2]]))
 
