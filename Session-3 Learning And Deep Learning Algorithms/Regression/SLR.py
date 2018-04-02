73ac68325119065a1028375925bb65c32e8580a7
prateek951 <prince.cenation@gmail.com>
2018-04-02 23:41:45 +0530

    performed linear regression on property prices in a random area in Delhi

--- Session-3 Learning And Deep Learning Algorithms/Regression/SLR.py
+++ Session-3 Learning And Deep Learning Algorithms/Regression/SLR.py
@@ -5,10 +5,25 @@ from scipy import stats
 # Feature Vector
 sq_ft = np.array([150,200,250,300,350,400,600])
 # Dependent Vector/Variable
-price = np.array([6450,7450,8450,9450,11450,15450,18450])
+price = np.array([55000000,95000000,12000000,16000000,27000000,30000000,57000000])
 
 slope,intercept,r_value,p_value,stderr =stats.linregress(sq_ft,price)
 
 # Trace the scatter plot for this to visualise
 plt.scatter(sq_ft,price,color='red',marker='*')
 plt.show()
+
+# Trace the scatter plot and pass a line through these points so to visualise the accuracy of our model
+plt.scatter(sq_ft,price,color='blue',marker='o')
+plt.plot(sq_ft,slope*sq_ft+intercept,color='black')
+plt.show()
+
+print(r_value**2)
+
+new_x = 550
+new_y = new_x*slope + intercept
+print(new_y)
+
+# The property price for a random area
+# where the square feet is 550 is 36 crores around
+# using linear regression
