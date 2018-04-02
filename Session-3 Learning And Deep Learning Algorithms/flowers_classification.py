53b5d89f9f270d01313ddc0c7c50a6f42a37fb5a
prateek951 <prince.cenation@gmail.com>
2018-04-02 22:15:04 +0530

    performed k-fold cross validation on ds

--- Session-3 Learning And Deep Learning Algorithms/flowers_classification.py
+++ Session-3 Learning And Deep Learning Algorithms/flowers_classification.py
@@ -96,7 +96,57 @@ dt_classifier = DecisionTreeClassifier()
 dt_classifier.fit(training_inputs,training_classes)
 
 # Validate the classifier on the testing set based on the accuracy of classification
-print(dt_classifier.predict(testing_inputs,testing_classes))
+print(dt_classifier.score(testing_inputs,testing_classes))
+
+model_accuracies = []
+
+for i in range(1000):
+    (training_inputs,training_classes,testing_inputs,testing_classes) = train_test_split(all_inputs,all_classes,test_size=0.75)
+
+    # DECISION TREE CLASSIFIER ALGORITHM
+       # Create the decision tree classifier
+        decision_tree_classifier = DecisionTreeClassifier()
+       # Train the classifier on the training set
+       decision_tree_classifier.fit(training_inputs,training_classes)
+       # Validate the classifier on the test set based on the accuracy of classification
+       accuracy = decision_tree_classifier(testing_inputs,testing_classes)
+        model_accuracies.append(accuracy)
+
+print(sb.distplot(model_accuracies))
+
+
+# This problem is the main reason that most data scientists perform k-fold cross-validation on their models: Split the original data set into k subsets, use one of the subsets as the testing set, and the rest of the subsets are used as the training set. This process is then repeated k times such that each subset is used as the testing set exactly once.
+
+# Performing the 10 fold cross validation on the dataset
+import numpy as np 
+from sklearn.cross_validation import StratifiedKFold
+
+def plot_cv(cv,n_samples):
+    masks=[]
+    for train,test in cv:
+        mask = np.zeros(n_samples,dtype=bool)
+        mask[test] = 1
+        masks.append(mask)
+    plt.figure(figsize=(15,15))
+    plt.imshow(masks,interpolation=None)
+    plt.ylabel('Fold')
+    plt.xlabel('Row #')
+plot_cv(StratifiedKFold(all_classes,n_folds=10),len(all_classes))
+
+# Perform 10 fold cross validation code on our model with the following code
+
+from sklearn.cross_validation import cross_val_score
+
+decision_tree_classifier = DecisionTreeClassifier()
+
+# Get the list of scores to get a reasonable estimate of the classifier's performance
+
+cv_scores = cross_val_score(decision_tree_classifier,all_inputs,all_classes,cv=10)
+print(sb.distplot(cv_scores))
+plt.title('Average Score : {}'.format(np.mean(cv_scores)))
+
+
+
 
 
 
